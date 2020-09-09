15a14f661e28c5dc47f2bc15471f93ecd2dcb460
maybinnun@gmail.com <maybinnun@gmail.com>
2020-09-09 12:36:34 +0300

    first states

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Login from './components/Pages/Login/Login'
 import {Route, Switch} from 'react-router-dom';
 import Toolbar from "./components/Pages/Toolbar/Toolbar";
@@ -10,19 +10,40 @@ import Checkout from "./components/Pages/Checkout/Checkout";
 import backround from "./assets/backround.jpg";
 
 
-function App() {
-  return (
-    <div style={{ backgroundImage:`url(${backround})`, position: 'fixed', minWidth: "100%", minHeight: '100%', backgroundSize: 'cover', backgroundPosition:'center'}}>
-        <Route path={"/"} component={Toolbar}/>
-        <Switch>
-            <Route path={"/Login"} component={Login}/>
-            <Route path={"/Menu"} component={PlantMenu}/>
-            <Route path={"/Signup"} component={Signup}/>
-            <Route path={"/Admin"} component={Admin}/>
-            <Route path={"/Checkout"} component={Checkout}/>
-      </Switch>
-      </div>
-  );
+export default class App extends Component {
+
+    state = {
+        isLoggedIn: false,
+        isAdmin: false,
+        selectedProducts:[]
+    };
+
+    setLoggedin = (isAdmin) => {
+        this.setState({isLoggedIn : true});
+        if (isAdmin) {
+            this.setState({isAdmin : true});
+        }
+    };
+
+    setLoggedOut = () => {
+        this.setState({isLoggedIn : false})
+    };
+
+    render() {
+        return (
+            <div style={{ padding:40, backgroundImage:`url(${backround})`, minWidth: "100%", minHeight: '1000px', backgroundSize: 'cover', backgroundPosition:'center'}}>
+                <Toolbar isLoggedIn={this.state.isLoggedIn} isAdmin={this.state.isAdmin} setLogOut={this.setLogOut} />
+                <Switch>
+                    <Route path={"/Login"} render={() => <Login setLoggedin={this.setLoggedin} />} />
+                    <Route path={"/Menu"} component={PlantMenu}/>
+                    <Route path={"/Signup"} component={Signup}/>
+                    <Route path={"/Admin"} component={Admin}/>
+                    <Route path={"/Checkout"} component={Checkout}/>
+                </Switch>
+            </div>
+        );
+    }
+
+
 }
 
-export default App;
